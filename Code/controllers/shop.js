b7fdefc5ff729ef745a55ec8e4a1c1409b28cb0b
thehashmap <nkmnikhil0009@gmail.com>
2024-01-19 15:09:15 +0530

    enhance the app

---
+++ Code/controllers/shop.js
@@ -0,0 +1,42 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      pageTitle: "All Products",
+      path: "/products",
+      prods: products,
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      pageTitle: "Shop",
+      path: "/",
+      prods: products,
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    pageTitle: "Your Orders",
+    path: "/orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout",
+  });
+};
